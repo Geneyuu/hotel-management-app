feb175c4f6e3cd9c9c1767ffd572455ce09546f1
Eugene Escario <99122875+Geneyuu@users.noreply.github.com>
2025-11-04 01:33:11 +0800

    just added a table header for rooms

--- src/pages/Rooms.jsx
+++ src/pages/Rooms.jsx
@@ -16,8 +16,18 @@ const Rooms = () => {
 	}, []);
 
 	return (
-		<div>
+		<div className="flex flex-col gap-4">
 			<h1>All Rooms</h1>
+			<table className="border  border-gray-700 w-full text-xs">
+				<thead className="bg-gray-800">
+					<tr className="flex justify-around p-3 uppercase rounded-2xl">
+						<th>Rooms</th>
+						<th>Price</th>
+						<th>Capacity</th>
+						<th>Discounts</th>
+					</tr>
+				</thead>
+			</table>
 		</div>
 	);
 };
